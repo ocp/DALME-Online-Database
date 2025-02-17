1ab37809cc863dca8f18716c5b29e597500d3948
jhrr <jhrr@users.noreply.github.com>
2025-02-17 17:56:14 +0000

    Correct fs read paths to static snippet files

--- app/domain/api/resources/pages/endpoints.py
+++ app/domain/api/resources/pages/endpoints.py
@@ -15,7 +15,7 @@ from domain.models import Page
 
 from .serializers import PageSerializer
 
-with pathlib.Path(f'{settings.PROJECT_ROOT}/app/static/snippets/iiif_manifest.json').open() as fp:
+with pathlib.Path(f'{settings.PROJECT_ROOT}/static/snippets/iiif_manifest.json').open() as fp:
     MANIFEST = json.load(fp)
 
 
