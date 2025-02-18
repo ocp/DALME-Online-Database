6787b4a8c336a5f214b2729ee9d6105fb31d6290
jhrr <jhrr@users.noreply.github.com>
2025-02-18 21:20:25 +0000

    Indicate to the storage backend where the manifest should be

--- app/tenants/storage_backends.py
+++ app/tenants/storage_backends.py
@@ -2,6 +2,7 @@
 
 from storages.backends.s3boto3 import S3Boto3Storage, S3ManifestStaticStorage
 
+from django.contrib.staticfiles.storage import StaticFilesStorage
 from django.db import connection
 
 from app.context import get_current_tenant
@@ -11,6 +12,10 @@ class StaticStorage(S3ManifestStaticStorage):
     """Multitenant aware staticfiles storage class for S3."""
 
     key = 'static'
+
+    def __init__(self, *args, **kwargs):
+        manifest_storage = StaticFilesStorage(location=self.location)
+        super().__init__(*args, manifest_storage=manifest_storage, **kwargs)
 
     @property
     def schema(self):
